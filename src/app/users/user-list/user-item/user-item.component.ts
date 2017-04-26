12c32bfc3be0632e357868c64fde4fe9105d78dd
Farouk Errahmani <farouk.errahmani@gmail.com>
2017-04-26 07:33:50 +0200

    init commit

---
+++ src/app/users/user-list/user-item/user-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../../user.model';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css']
+})
+export class UserItemComponent implements OnInit {
+
+  @Input('user-item') user: User;
+  @Input() index: number;
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/users/user-list/user-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user.model';
+import { UsersService } from '../users.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  users: User[];
+
+  constructor(private userService: UsersService) { }
+
+  ngOnInit() {
+    this.users = this.userService.getUsers();
+  }
+
+}
